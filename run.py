6ab2ade2f23141a74c70ed2f60106321455909b8
Surita <castro@Nasa.home>
2016-07-05 12:27:05 -0300

    corrige footers de login

--- run.py
+++ run.py
@@ -1,10 +1,8 @@
-from app import app
+import app
 import mapping
 import models
-
-wsgi = app.wsgifunc()
 
 if __name__ == "__main__":
   models.createDB()
   app.mapStatic(app.StaticDirs)
-  app.run()
+  app.app.run()
